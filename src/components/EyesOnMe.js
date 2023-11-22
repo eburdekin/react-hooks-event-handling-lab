7284502dc9e735d05a6191e2447787b7a6e6293c
Eileen <eileenburdekin@proton.me>
2023-11-21 21:29:23 -0800

    complete lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe() {
+    return <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+}
+
+function handleFocus() {
+    console.log('Good!')
+}
+
+function handleBlur() {
+    console.log('Hey! Eyes on me!')
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -2,8 +2,17 @@
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <form>
+                <input type="password" onChange={handleChange}></input>
+                <input type="submit"></input>
+            </form>
+        </div>
     )
+}
+
+function handleChange() {
+    console.log('Entering password...')
 }
 
 export default Keypad;
